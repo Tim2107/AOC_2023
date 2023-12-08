018ee70203ce9615b2efe682e7c576145f3970a1
Martin Nathaniel <martin.t.nathaniel@gmail.com>
2023-12-08 17:34:21 +0100

    calling the module for day 8 part 1 from main

--- src/day_8/mod.rs
+++ src/day_8/mod.rs
@@ -1,3 +1,20 @@
 mod node;
 mod graph;
 mod parser;
+
+pub fn solve_day_8_part_1() -> usize{
+    let input_file = "resources\\input_day_8.txt";
+    let file_content = parser::read_file(input_file)
+                                .expect("Failed to read test file");
+    let cycle_instructions = parser::parse_cycle_instruction(&file_content)
+                                        .expect("Failed to parse cycle instructions");
+    let nodes = parser::parse_nodes(&file_content)
+                                .expect("Failed to parse nodes");
+    let mut graph = graph::Graph::new();
+    for node in nodes {
+        graph.add_node(node);
+    }
+    let traversal_steps = graph.traverse("AAA", "ZZZ", &cycle_instructions, 10)
+                                            .expect("Traversal failed");
+    traversal_steps.1
+}
