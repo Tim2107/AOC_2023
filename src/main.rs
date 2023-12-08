19632fdb3f5cd355ca063664e0db69f964196d63
Martin Nathaniel <martin.t.nathaniel@gmail.com>
2023-12-08 17:41:34 +0100

    fixed cycle - logic

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ fn main() {
 
     let solution_day_8_part_1 = day_8::solve_day_8_part_1();
 
-    println!("The solution to day 8 - part 1 is {} steps", solution_day_8_part_1);
+    println!("The solution to day 8 - part 1 is {} steps.", solution_day_8_part_1);
 }
