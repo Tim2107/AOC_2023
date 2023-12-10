d9b451cc6538580eafd4d6c145c8788eb14dfdd5
Martin Nathaniel <martin.t.nathaniel@gmail.com>
2023-12-10 19:20:32 +0100

    day 9 sequence_analysis prototyping

--- src/day_9/mod.rs
+++ src/day_9/mod.rs
@@ -1,2 +1,3 @@
 mod parser;
 mod errors;
+mod sequence_analysis;
---
+++ src/day_9/sequence_analysis.rs
@@ -0,0 +1,81 @@
+use crate::day_9;
+
+pub fn extrapolate_next_number(sequences: &[Vec<i32>]) -> i32 {
+    let mut next_number = 0;
+    for sequence in sequences.iter().rev() {
+        next_number += sequence.last().copied().unwrap_or_default();
+    }
+
+    next_number
+}
+
+pub fn calculate_complete_difference_sequences(sequence: &[i32]) -> Vec<Vec<i32>> {
+    let mut sequences = vec![sequence.to_vec()];
+    let mut current_sequence = sequence.to_vec();
+
+    while current_sequence.iter().any(|&x| x != 0) {
+        current_sequence = calculate_differences(&current_sequence);
+        sequences.push(current_sequence.clone());
+    }
+
+    sequences
+}
+
+pub fn calculate_differences(sequence: &[i32]) -> Vec<i32> {
+    sequence.windows(2)
+        .map(|window| window[1] - window[0])
+        .collect()
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    use day_9::parser::parse_sequences_from_file;
+
+    #[test]
+    fn test_calculate_differences(){
+        let expected_differences = vec![3,3,3,3,3];
+
+        let sequences_to_analyze = parse_sequences_from_file("resources/input_day_9_test.txt").unwrap();
+        let calculated_differences = calculate_differences(sequences_to_analyze.first().unwrap());
+
+        assert_eq!(calculated_differences, expected_differences);
+    }
+
+    #[test]
+    fn test_calculate_complete_difference_sequences(){
+        let original_sequence = vec![0,3,6,9,12,15];
+        let expected_differences_1 = vec![3,3,3,3,3];
+        let expected_differences_2 = vec![0,0,0,0];
+
+        let sequences_to_analyze = parse_sequences_from_file("resources/input_day_9_test.txt").unwrap();
+        let calculated_sequences = calculate_complete_difference_sequences(sequences_to_analyze.first().unwrap());
+
+        assert_eq!(calculated_sequences.len(),3);
+        assert!(calculated_sequences.contains(&original_sequence));
+        assert!(calculated_sequences.contains(&expected_differences_1));
+        assert!(calculated_sequences.contains(&expected_differences_2));
+    }
+
+    #[test]
+    fn test_extrapolate_next_number_a(){
+        test_extrapolate_next_number_helper(0,18);
+    }
+
+    #[test]
+    fn test_extrapolate_next_number_b(){
+        test_extrapolate_next_number_helper(1,28);
+    }
+
+    #[test]
+    fn test_extrapolate_next_number_c(){
+        test_extrapolate_next_number_helper(2,68);
+    }
+
+    fn test_extrapolate_next_number_helper(test_sequence: usize, expected_result: i32){
+        let sequences_to_analyze = parse_sequences_from_file("resources/input_day_9_test.txt").unwrap();
+        let calculated_sequences = calculate_complete_difference_sequences(&sequences_to_analyze[test_sequence]);
+        let extrapolated_number = extrapolate_next_number(&calculated_sequences);
+        assert_eq!(extrapolated_number, expected_result);
+     }
+}
