8ad6906f74086c0174e69a6b71876680aeeb2a31
Martin Nathaniel <martin.t.nathaniel@gmail.com>
2023-12-24 15:14:16 +0100

    refactored day 10 parser tests

--- src/day_10/parser.rs
+++ src/day_10/parser.rs
@@ -20,16 +20,19 @@ impl Parser {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use rstest::rstest;
     use super::*;
     use crate::utils::input_output::read_file;
 
-    #[test]
-    fn test_read_file() {
-        let content = read_file("resources/input_day_10_test_a.txt").unwrap();
+    #[rstest]
+    #[case("resources/input_day_10_test_a.txt")]
+    #[case("resources/input_day_10_test_b.txt")]
+    #[case("resources/input_day_10_test_c.txt")]
+    #[case("resources/input_day_10_test_d.txt")]
+    fn test_read_file(#[case] input_file: &str) {
+        let content = read_file(input_file).unwrap();
         assert!(!content.is_empty());
     }
 
