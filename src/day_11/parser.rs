1f6c4ad5c86770c4d3e1515813d97820028d3b8d
Martin Nathaniel <martin.t.nathaniel@gmail.com>
2023-12-28 00:44:14 +0100

    day_11 part 1 prototyping  refactor

--- src/day_11/parser.rs
+++ src/day_11/parser.rs
@@ -1,3 +1,8 @@
+enum Dimension {
+    Row,
+    Column,
+}
+
 pub struct Parser{
     cosmos: Vec<Vec<char>>,
 }
@@ -10,40 +15,62 @@ impl Parser {
                                     .collect(),
         }
     }
-    pub fn adjust_for_cosmic_expansion(&mut self) -> &Vec<Vec<char>> {
-        self.double_rows_of_empty_space();
-        self.double_columns_of_empty_space();
-        &self.cosmos
+
+    pub fn adjust_for_cosmic_expansion(&mut self) {
+        self.double_at_index_if_empty_space(Dimension::Row);
+        self.double_at_index_if_empty_space(Dimension::Column);
     }
 
-    fn double_rows_of_empty_space(&mut self) {
-        let mut row = 0;
-        while row < self.cosmos.len() {
-            if self.cosmos[row].iter().all(|&column| column == '.') {
-                self.cosmos.insert(row + 1, self.cosmos[row].clone());
-                row += 1;
+    fn double_at_index_if_empty_space (&mut self, dimension: Dimension) {
+        let mut index : usize = 0;
+        while match dimension { Dimension::Row =>       self.is_below_row_size(index),
+                                Dimension::Column =>    self.is_below_column_size(index) }
+        { if match dimension  { Dimension::Row =>       self.is_empty_row(index),
+                                Dimension::Column =>    self.is_empty_column(index) }
+            {
+                match dimension  { Dimension::Row =>       self.double_row(index),
+                                   Dimension::Column =>    self.double_column(index) }
+                index +=1;
             }
-            row += 1;
+            index +=1;
+        }
+
+    }
+
+    fn is_empty_row(&self, row: usize) -> bool {
+        self.cosmos[row].iter().all(|&column| column == '.')
+    }
+
+    fn is_empty_column(&self, column: usize) -> bool {
+        self.cosmos.iter().all(|row| row[column] == '.')
+    }
+
+    fn double_row(&mut self, row: usize) {
+        self.cosmos.insert(row + 1, self.cosmos[row].clone());
+    }
+
+    fn double_column(&mut self, column: usize) {
+        for row in &mut self.cosmos {
+            row.insert(column + 1, '.');
         }
     }
 
-    fn double_columns_of_empty_space(&mut self) {
-        let mut column = 0;
-        while column < self.cosmos[0].len() {
-            if self.cosmos.iter().all(|row| row[column] == '.') {
-                for row in &mut self.cosmos {
-                    row.insert(column + 1, '.');
-                }
-                column += 1;
-            }
-            column += 1;
-        }
+    fn is_below_row_size(&self, row: usize) -> bool {
+        row < self.cosmos.len()
+    }
+
+    fn is_below_column_size(&self, column: usize) -> bool {
+        column < self.cosmos[0].len()
+    }
+
+    fn catalogue_galaxies(&mut self){
+        
     }
 }
 
 #[cfg(test)]
 mod tests{
-    use rstest::rstest;
+    use rstest::*;
     use super::*;
     use crate::utils::input_output::{read_file};
 
@@ -52,19 +79,77 @@ mod tests{
     #[case("resources/input_day_11_test_b.txt")]
     #[case("resources/input_day_11_test_c.txt")]
     #[case("resources/input_day_11_test_d.txt")]
+
     fn test_read_file(#[case] input_file: &str) {
         let content = read_file(input_file).unwrap();
         assert!(!content.is_empty());
     }
 
+    #[fixture]
+    pub fn test_cosmos() -> String {
+        "..#\n...\n#..".to_string()
+    }
+
+    #[rstest]
+    pub fn test_is_empty_row_positive(test_cosmos: String) {
+        let parser = Parser::new(&test_cosmos);
+        assert!(parser.is_empty_row(1));
+    }
+
+    #[rstest]
+    pub fn test_is_empty_row_negative(test_cosmos: String) {
+        let parser = Parser::new(&test_cosmos);
+        assert!(!parser.is_empty_row(0));
+    }
+
+    #[rstest]
+    pub fn test_is_empty_column_positive(test_cosmos: String) {
+        let parser = Parser::new(&test_cosmos);
+        assert!(parser.is_empty_column(1));
+    }
+
+    #[rstest]
+    pub fn test_is_empty_column_negative(test_cosmos: String) {
+        let parser = Parser::new(&test_cosmos);
+        assert!(!parser.is_empty_column(0));
+    }
+
+
+    #[rstest]
+    pub fn test_double_columns_of_empty_space(test_cosmos: String) {
+      let expected_cosmos = vec![vec!['.', '.','.','#'],
+                                 vec!['.', '.','.','.'],
+                                 vec!['#', '.','.','.']];
+
+        let mut parser = Parser::new(&test_cosmos);
+        parser.double_at_index_if_empty_space(Dimension::Column);
+        assert_eq!(parser.cosmos, expected_cosmos);
+    }
+
+    #[rstest]
+    pub fn test_double_rows_of_empty_space(test_cosmos: String) {
+        let expected_cosmos = vec![vec!['.', '.', '#'],
+                                   vec!['.', '.', '.'],
+                                   vec!['.', '.', '.'],
+                                   vec!['#', '.', '.']];
+
+        let mut parser = Parser::new(&test_cosmos);
+        parser.double_at_index_if_empty_space(Dimension::Row);
+        assert_eq!(parser.cosmos, expected_cosmos);
+    }
+
     #[test]
     fn test_adjust_for_cosmic_expansion() {
+
         let observatory_data = read_file("resources/input_day_11_test_a.txt").unwrap();
         let mut parser = Parser::new(&observatory_data);
-        let expanded_cosmos = parser.adjust_for_cosmic_expansion();
+        parser.adjust_for_cosmic_expansion();
+        let expanded_cosmos = parser.cosmos;
+
         let expected_cosmos_data = read_file("resources/input_day_11_test_b.txt").unwrap();
         let expected_data_parser = Parser::new(&expected_cosmos_data);
         let expected_cosmos = expected_data_parser.cosmos;
-        assert_eq!(*expanded_cosmos, expected_cosmos);
+
+        assert_eq!(expanded_cosmos, expected_cosmos);
     }
 }
